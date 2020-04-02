f82e235a9a18e31ab4cd5bd5f07586f13456376f
Andrzej G <glogo85@gmail.com>
2020-04-02 23:35:09 +0200

    - add country icons, replaced href links with methods
    - replaced href links with methods
    - added country flags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'JoannaDamPhotography';
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  private openFacebook() {
+    window.open('https://www.facebook.com/joanna.dam.2019', '_blank');
+  }
+
+  private openInstagram() {
+    window.open('https://www.instagram.com/dreamcatcher.by.sia', '_blank');
+  }
+
 }
